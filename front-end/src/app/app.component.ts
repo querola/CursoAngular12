5f211a2406938fe53a8846d8ac8b4d74ff1e2d27
DesarrolloProsis <rmendoza@grupo-prosis.com>
2021-10-04 12:55:38 -0500

    todo bien hasta la clase 24

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.peliculas = [
+        this.peliculas = {
+          titulo: 'spider Man',
+          fechaLanzamiento: new Date(),
+          precio: 1244.99
+        },
+        this.peliculas = {
+          titulo: 'Thor',
+          fechaLanzamiento: new Date(),
+          precio: 1099.99
+        }]
+    }, 1000);
+    throw new Error('Method not implemented.');
+  }
   title = 'el valor que yo quiera';
 
-  pelicula = {
-    titulo: 'spider Man',
-    fechaLanzamiento: new Date(),
-    precio: 1244.99
-  };
+  peliculas;
 
   duplicarNumero(valor: number): number{
     return valor * 2;
