f197f598408179531de819e4e6cbb842c0ccda43
Rodrigo Mendoza <el_stroke_666@hotmail.com>
2021-10-13 12:32:15 -0500

    Todo bien hasta la clase 48

--- front-end/src/app/material/material.module.ts
+++ front-end/src/app/material/material.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatIcon, MatIconModule } from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+
 @NgModule({
   declarations: [],
   exports: [
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   imports: [
     CommonModule
